8b422957d91ef9671ad8e997da5d570312281134
Ryan G Ferris <ryanglenferris@gmail.com>
2017-12-15 15:22:24 -0800

    materialize to bootstrap-v4 styles library
    remove references to materialize style rules in the index.html header, reference Bootstrap v4 in the header, and update class names to match bootstrap nomaclature.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,30 @@
 /* BACK END LOGIC */
-
 function Pizza() {
   this.type = "cheese";
   this.sizes = [];
   this.toppings = [];
   this.price;
 }; // End of 'Pizza' constructor.
-
 function Size(diameter, price) {
   this.diameter = diameter;
   this.price = price;
 }; // End of 'Size' constructor.
-
 function Topping(topping, price) {
   this.topping = topping;
   this.price = price;
 }; // End of 'Topping' constructor.
-
 Pizza.prototype.pushSizesArray = function(size) {
 	this.sizes.push(size);
 }; // End of Pizza 'pushSizesArray' prototype.
-
 Pizza.prototype.pushToppingsArray = function(topping) {
   this.toppings.push(topping);
 }; // End of Pizza 'pushToppingsArray' prototype.
-
 Pizza.prototype.priceToppings = function () {
   var lengthToppings = this.toppings.length;
   for(i = 0; i < lengthToppings; i++) {
     this.price += this.toppings[i].price;
   }; // End of the 'Pizza.toppings[]' for-loop.
 }; // End of the Pizza 'priceToppings' prototype.
-
 Pizza.prototype.priceSize = function () {
   var lengthSizes = this.sizes.length;
   for(i = 0; i < lengthSizes; i++) {
@@ -40,3 +33,16 @@ Pizza.prototype.priceSize = function () {
 };
 
 /* FRONT END LOGIC */
+$(document).ready(function(){
+  var
+  
+
+
+
+  $("#pizza-form").submit(function(event){
+    event.preventDefault();
+    var selectedSize = ;
+    var selectedToppings = ;
+
+}); // End of the document 'ready' listener.
+// });
