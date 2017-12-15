bc2abc3907912f5213958477bc944a4c22b67b03
Ryan G Ferris <ryanglenferris@gmail.com>
2017-12-15 09:43:02 -0800

    pizza-constructor
    create back end logic for the Pizza object constructor.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/* BACK END LOGIC */
+/* variable/array notes
+toppings: ["anchovy", "artichoke", "cheese", "pepperoni", "pineapple", "mushroom"];
+size: ["12-inch", "15-inch", "18-inch"];
+*/
+//Global variables
+function Pizza() {
+  this.type = "cheese";
+  this.sizes = [];
+  this.toppings = [];
+}; // End of 'Pizza' constructor.
+
+
+
+
+/* FRONT END LOGIC */
