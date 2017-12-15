8b1cb359643051e09b45c7b15684fc6e675e465b
Ryan G Ferris <ryanglenferris@gmail.com>
2017-12-15 13:23:04 -0800

    pizza.sizes-price
    add back end prototype to calculate pizza size prices.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,42 @@
 /* BACK END LOGIC */
-// Constructor for the 'Pizza' object.
+
 function Pizza() {
   this.type = "cheese";
   this.sizes = [];
   this.toppings = [];
   this.price;
 }; // End of 'Pizza' constructor.
-// Constructor for the 'Size' object.
+
 function Size(diameter, price) {
   this.diameter = diameter;
   this.price = price;
 }; // End of 'Size' constructor.
-// Constructor for the 'Topping' object.
+
 function Topping(topping, price) {
   this.topping = topping;
   this.price = price;
 }; // End of 'Topping' constructor.
-// Prototype to push the 'Size' object to the 'Pizza.sizes' array.
+
 Pizza.prototype.pushSizesArray = function(size) {
 	this.sizes.push(size);
 }; // End of Pizza 'pushSizesArray' prototype.
-// Prototype to push the 'Topping' object to the 'Pizza.toppings' array.
+
 Pizza.prototype.pushToppingsArray = function(topping) {
   this.toppings.push(topping);
 }; // End of Pizza 'pushToppingsArray' prototype.
-// Prototype for the 'Pizza' object that will
+
 Pizza.prototype.priceToppings = function () {
   var lengthToppings = this.toppings.length;
   for(i = 0; i < lengthToppings; i++) {
-    this.price += this.toppings[i].prices
+    this.price += this.toppings[i].price;
   }; // End of the 'Pizza.toppings[]' for-loop.
 }; // End of the Pizza 'priceToppings' prototype.
 
+Pizza.prototype.priceSize = function () {
+  var lengthSizes = this.sizes.length;
+  for(i = 0; i < lengthSizes; i++) {
+    this.price += this.sizes[i].price;
+  }; // End of the 'Pizza.sizes[]' for-loop.
+};
 
 /* FRONT END LOGIC */
