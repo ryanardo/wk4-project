851297ea95df6f1c4dba7d42724d03e8da64f36c
Ryan G Ferris <ryanglenferris@gmail.com>
2017-12-17 09:44:51 -0800

    pizza-form submit
    create front end jQuery logic for the submit for event.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/* BACK END LOGIC */
+function Pizza(size, price) {
+  this.size =
+  this.toppings = [];
+  this.price
+
+}
+
+
+
+/* FRONT END LOGIC */
+$("#pizza-form").submit(function(event) {
+  event.preventDefault();
+
+}); // End of 'pizza-form' submit listener.
